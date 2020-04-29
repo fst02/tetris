6ad4fb91498ba0efa1f22fa5c690199611964c77
bh-fst02 <bh.fst02@gmail.com>
2020-04-29 10:53:35 +0200

    fix randomitem

--- script.js
+++ script.js
@@ -7,72 +7,59 @@ let columns = 10;
 const canvasWidth = 209;
 const canvasHeight = 419;
 const rectSize = (canvasWidth - columns + 1) / columns;
-const itemT = [
+const black = '#000000';
+const gray = '#808080';
+
+const items = [ [
     [1, 1, 1],
     [0, 1, 0]
-];
-const itemReverseZ = [
+],
+
+[
     [0, 1, 1],
     [1, 1, 0]
-];
-const itemZ = [
+],
+
+[
     [1, 1, 0],
     [0, 1, 1]
-];
-const itemReverseL = [
+],
+
+[
     [1, 0, 0],
     [1, 1, 1]
-];
-const itemL = [
+],
+
+[
     [0, 0, 1],
     [1, 1, 1]
-];
-const itemLine = [
+],
+
+[
     [1, 1, 1, 1]
-];
-const itemRect = [
+],
+
+[
     [1, 1],
     [1, 1]
-];
+]];
 
 function randomItem() {
     let randomNumber = Math.floor(Math.random() * 7);
-    switch (randomNumber) {
-      case 0:
-        return itemT;
-        break;
-      case 1:
-        return itemReverseZ;
-        break;
-      case 2:
-        return itemZ;
-        break;
-      case 3:
-        return itemReverseL;
-        break;
-      case 4:
-        return itemL;
-        break;
-      case 5:
-        return itemLine;
-        break;
-      case 6:
-        return itemRect;
-        break;  
-      default:
-        break;
-    }
-  }
+    return items[randomNumber];
+    //console.log(randomNumber);
+}
+
   
 function recolor() {
     for (let i = 0; i < arrCanvas.length; i++) {
         for (let j = 0; j < arrCanvas[i].length; j++) {
             if (arrCanvas[i][j] === 1) {
-                ctx.fillStyle = '#000000';
+                ctx.fillStyle = black;
                 ctx.fillRect(j * (rectSize + 1),i * (rectSize + 1),rectSize,rectSize);
             }
             else {
-                ctx.fillStyle = '#808080';
+                ctx.fillStyle = gray;
                 ctx.fillRect(j * (rectSize + 1),i * (rectSize + 1),rectSize,rectSize);
             }
         }
@@ -98,7 +85,7 @@ function colorGameField(arrCanvas, rows, columns) {
         for (let j = 0; j < columns; j++) {
             arrCanvas[i][j] = 0;
             if(arrCanvas[i][j] === 0) {
-                ctx.fillStyle = '#808080';
+                ctx.fillStyle = gray;
                 ctx.fillRect(j * (rectSize + 1),i * (rectSize + 1),rectSize,rectSize);
             }
         }
