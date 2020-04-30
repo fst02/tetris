35dedf45763ee676d19c405e87b53828555c47a7
bh-fst02 <bh.fst02@gmail.com>
2020-04-30 16:42:34 +0200

    #FST2-24 WIP - controll falling blocks

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const arrCanvas = [];
 const rows = 20;
 const columns = 10;
 const canvasWidth = 209;
@@ -14,77 +13,23 @@ let offset;
 const initialOffset = 3;
 let offsetVertical = 0;
 
-function setGravity() {
-  offsetVertical = 0;
-  let interval = setInterval(function() {
-    if (!hasConflict(bottomBorder, element)) {
-      resetArr(arrCanvas);
-      positionItem(offset);
-      offsetVertical++
-    } else {
-      clearInterval(interval);
-      console.log('ok');
-    }
-  }, 1000);
-}
-
-function positionItem(positionNumber) {
-  for (let i = 0; i < randomItemResult.length; i++) {
-    for (let j = 0; j < randomItemResult[i].length; j++) {
-      if (randomItemResult[i][j] === 1) {
-        arrCanvas[i + offsetVertical][j + positionNumber] = 1;
-      }
-    }
-  }
-  recolor();
-  updateElement();
-}
-
-function hasConflict(display1, display2) {
-  for (let i = 0; i < display1.length; ++i) {
-    for (let j = 0; j < display1[i].length; ++j) {
-      if (display1[i][j] === 1 && display2[i][j] === 1) {
-        return true;
-      }
-    }
-  }
-}
-
-function updateElement() {
-  resetArr(element);
-  for (let i = 0; i < randomItemResult.length; i++) {
-    for (let j = 0; j < randomItemResult[i].length; j++) {
-      if (randomItemResult[i][j] === 1) {
-        element[i + offsetVertical][j + offset] = 1;
-      }
-    }
-  }
-}
-
-function moveHorizontally() {
-  document.addEventListener('keydown', (event) => {
-    if (event.key === 'ArrowLeft' && !hasConflict(element, leftBorder)) {
-      offset -= 1;
-      setGameField();
-      positionItem(offset);
-    }
-    if (event.key === 'ArrowRight' && !hasConflict(element, rightBorder)) {
-      offset += 1;
-      setGameField();
-      positionItem(offset);
-    }
-  });
-}
-
 function randomItem() {
-  const randomNumber = Math.floor(Math.random() * 7);
+  const randomNumber = Math.floor(Math.random() * items.length);
   return items[randomNumber];
 }
 
-function recolor() {
-  for (let i = 0; i < arrCanvas.length; i++) {
-    for (let j = 0; j < arrCanvas[i].length; j++) {
-      if (arrCanvas[i][j] === 1) {
+function resetArr(display) {
+  for (let i = 0; i < display.length; i++) {
+    for (let j = 0; j < display[i].length; j++) {
+      display[i][j] = 0;
+    }
+  }
+}
+
+function recolor(display) {
+  for (let i = 0; i < display.length; i++) {
+    for (let j = 0; j < display[i].length; j++) {
+      if (display[i][j] === 1) {
         ctx.fillStyle = black;
         ctx.fillRect(j * (rectSize + 1), i * (rectSize + 1), rectSize, rectSize);
       } else {
@@ -95,30 +40,119 @@ function recolor() {
   }
 }
 
-function resetArr(display1) {
-  for (let i = 0; i < display1.length; i++) {
-    for (let j = 0; j < display1[i].length; j++) {
-      display1[i][j] = 0;
+function recolorLines(display) {
+  for (let i = 0; i < display.length; i++) {
+    for (let j = 0; j < display[i].length; j++) {
+      if (display[i][j] === 1) {
+        ctx.fillStyle = black;
+        ctx.fillRect(j * (rectSize + 1), i * (rectSize + 1), rectSize, rectSize);
+      }
     }
   }
 }
 
 function firstItem() {
   randomItemResult = randomItem();
+  offsetVertical = 0;
   offset = initialOffset;
   positionItem(initialOffset);
-  setGravity();
 }
 
+function positionItem(positionNumber) {
+  for (let i = 0; i < randomItemResult.length; i++) {
+    for (let j = 0; j < randomItemResult[i].length; j++) {
+      if (randomItemResult[i][j] === 1) {
+        arrCanvas[i + offsetVertical][j + positionNumber] = 1;
+      }
+    }
+  }
+  recolor(arrCanvas);
+  recolorLines(lines);
+  resetArr(element);
+  updateDisplay(element);
+}
 
-function setGameField() {
-  for (let i = 0; i < rows; i++) {
-    arrCanvas[i] = [];
-    for (let j = 0; j < columns; j++) {
-      arrCanvas[i][j] = 0;
+function updateDisplay(display) {
+  for (let i = 0; i < randomItemResult.length; i++) {
+    for (let j = 0; j < randomItemResult[i].length; j++) {
+      if (randomItemResult[i][j] === 1) {
+        display[i + offsetVertical][j + offset] = 1;
+      }
     }
   }
 }
-setGameField();
+
+function moveHorizontally() {
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowLeft' && !hasConflict(element, leftBorder)) {
+      offset -= 1;
+      resetArr(arrCanvas);
+      positionItem(offset);
+    }
+    if (event.key === 'ArrowRight' && !hasConflict(element, rightBorder)) {
+      offset += 1;
+      resetArr(arrCanvas);
+      positionItem(offset);
+    }
+    if (event.key === 'ArrowDown') {
+      moveVertically();
+    }
+  });
+}
+
+function hasConflict(display1, display2) {
+  for (let i = 0; i < display1.length; ++i) {
+    for (let j = 0; j < display1[i].length; ++j) {
+      if (display1[i][j] === 1 && display2[i][j] === 1) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+// function setGravity() {
+//   offsetVertical = 0;
+//   let interval = setInterval(function() {
+//     console.log(hasConflict(bottomBorder, element))
+//     console.log(hasConflict(lines, element))
+//     if (!hasConflict(bottomBorder, element) && !hasConflict(lines, element)) {
+//       resetArr(arrCanvas);
+//       positionItem(offset);
+//       if (offsetVertical < element.length - randomItemResult.length) {
+//         offsetVertical++;
+//       }
+//     }
+//     else {
+//     clearInterval(interval);
+//     updateDisplay(lines);
+//     recolorLines(lines);
+//     firstItem();
+//     setGravity();
+//     console.log('OK');
+//     }
+//   }, 200);
+//   }
+offsetVertical = 0;
+function moveVertically() {
+  console.log(hasConflict(bottomBorder, element));
+  console.log(hasConflict(element, lines));
+  if (!hasConflict(bottomBorder, element) && !hasConflict(element, lines)) {
+    resetArr(arrCanvas);
+    positionItem(offset);
+    if (offsetVertical < element.length - randomItemResult.length) {
+      offsetVertical++;
+    }
+  }
+  else {
+  updateDisplay(lines);
+  recolorLines(lines);
+  firstItem();
+  console.log('OK');
+  }
+}
+
+recolor(arrCanvas);
 firstItem();
+//setGravity();
 moveHorizontally();
